8bd7b68d9adf11dec813619e3203cff3cb32bd24
Adarsh Danda <adarshdanda@wirelessprvnat-172-16-155-183.near.illinois.edu>
2024-10-15 00:09:23 -0500

    added post likelist route, need to fix user based post routes

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,14 @@ const PORT = process.env.PORT || 3000; // will be using set port later
 
 const userRoutes = require('./routes/user');
 const followingRoutes = require('./routes/followerInfo');
+const postRoutes = require('./routes/postInformation');
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('hi')
 });
 
+app.use('/postInfo', postRoutes);
 app.use('/followingInfo', followingRoutes);
 app.use('/users', userRoutes);
 
--- backend/routes/postInformation.js
+++ backend/routes/postInformation.js
@@ -1,28 +1,48 @@
-/* const express = require('express');
+const express = require('express');
 const router = express.Router();
-const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, QueryCommand, GetItemCommand } = require("@aws-sdk/client-dynamodb");
 const { awsConfig } = require('../config');
 
 
 const ddbClient = new DynamoDBClient(awsConfig);
 const dynamoTableName = 'Social';
 
-router.get('/:userID/following', async (req, res) => {
-    const params = {
+router.get('/:postID/likelist', async (req, res) => {
+  const params = {
       TableName: dynamoTableName,
-      Key: {
-          PK: {
-              S: req.params["userID"] + "#following"
-          },
-          SK: {
-              S: 'count'
+      ExpressionAttributeValues: {
+          ":pk": {
+              S: req.params["postID"] + "#likelist"
           }
-      }
-    };
-    await ddbClient.send(new GetItemCommand(params)).then(result => {
-        res.send(result.Item)
-      }).catch(error => {
-        res.status(500).send(error)
-      })
-  });
-*/
+      },
+      KeyConditionExpression: "PK = :pk",
+      ProjectionExpression: "SK"
+  };
+  await ddbClient.send(new QueryCommand(params)).then(result => {
+      res.send(result.Items)
+    }).catch(error => {
+      res.status(500).send(error)
+    })
+});
+/*
+Route below is currently not working, fix soon
+router.get('/:userID/:postID/posts', async (req, res) => {
+  const params = {
+    TableName: dynamoTableName,
+    Key: {
+        PK: {
+            S: req.params["userID"] + "#post"
+        },
+        SK: {
+            S: req.params["postID"]
+        }
+    }
+  };
+  await ddbClient.send(new GetItemCommand(params)).then(result => {
+      res.send(result.Item)
+    }).catch(error => {
+      res.status(500).send(error)
+    })
+});
+*/ 
+module.exports = router

cdc74831d0e807668a55262f4200d624c9c96e6a
Adarsh Danda <adarshdanda@wirelessprv-10-195-155-51.near.illinois.edu>
2024-10-25 18:43:41 -0500

    added unfollow route

--- backend/routes/followerInfo.js
+++ backend/routes/followerInfo.js
@@ -23,7 +23,7 @@ async function getFollowingList(req, res, userID) {
         const listOfUsers = result.Items;
         for (let i = 0; i < listOfUsers.length; i++) {
             followList.push(listOfUsers[i]["SK"]["S"]);
-        }
+        } // maybe change the follow List to a set here
         return followList;
     }).catch(error => {
         throw error;
@@ -152,6 +152,102 @@ router.post('/user/follow', async (req, res) => {
 
     await ddbClient.send(new TransactWriteItemsCommand(params)).then((result) => {
         return res.send(`Succesfully followed ${followingThisUser}`);
+    }).catch(error => {
+        return res.send(`An error occured: ${error}`);
+    });
+});
+
+
+router.post('/user/unfollow', async (req, res) => {
+    if (!req.session.username) {
+        return res.status(403).send("User must be logged in to follow");
+    }
+    const username = req.session.username;
+    const { unfollowThisUser } = req.body;
+    // may need to add a can't follow again implmentation
+    try {
+        const followingList = await getFollowingList(req, res, username);
+        const followingSet = new Set(followingList)
+        if (!followingSet.has(unfollowThisUser)) {
+            return res.send("You don't follow this user.");
+        }
+    }
+    catch (error) {
+        res.status(500).send("Some error occured:" + error);
+    }
+
+    const params = {
+        TransactItems: [
+            {
+                Delete: {
+                    TableName: dynamoTableName,
+                    Key: {
+                        PK: {
+                            S: username + "#following"
+                        },
+                        SK: {
+                            S: unfollowThisUser
+                        }
+                    }
+                }
+            }, 
+            {
+                Delete: {
+                    TableName: dynamoTableName,
+                    Key: {
+                        PK: {
+                            S: unfollowThisUser + "#followers"
+                        },
+                        SK: {
+                            S: username
+                        }
+                    }
+                }
+            },
+            {
+                Update: {
+                    TableName: dynamoTableName,
+                    Key: {
+                        PK: {
+                            S: username
+                        },
+                        SK: {
+                            S: "count"
+                        }
+                    },
+                    UpdateExpression: "ADD #following :inc",
+                    ExpressionAttributeNames: {
+                        "#following": "following#"  // Use a placeholder for attribute names
+                    },
+                    ExpressionAttributeValues: {
+                        ":inc": { N: "-1" },         // Increment value
+                    },
+                }
+            },
+            {
+                Update: {
+                    TableName: dynamoTableName,
+                    Key: {
+                        PK: {
+                            S: unfollowThisUser
+                        },
+                        SK: {
+                            S: "count"
+                        }
+                    },
+                    UpdateExpression: "ADD #follower :inc",
+                    ExpressionAttributeNames: {
+                        "#follower": "follower#"    // Use a placeholder for attribute names
+                    },
+                    ExpressionAttributeValues: {
+                        ":inc": { N: "-1" },         // Increment value
+                    },
+                }
+            }
+        ]
+    };
+    await ddbClient.send(new TransactWriteItemsCommand(params)).then((result) => {
+        return res.send(`Succesfully unfollowed ${unfollowThisUser}`);
     }).catch(error => {
         return res.send(`An error occured: ${error}`);
     });

72fb50e18a9f038b891d475b60a57e78b3184e2a
Adarsh Danda <adarshd2@illinois.edu>
2024-11-01 23:56:11 -0500

    Project end

---
+++ frontend/src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dashboard from '../components/Dashboard.js';
+
+function LandingPage() {
+    return (<div>
+        <Dashboard />
+    </div>);
+}
+
+export default LandingPage;

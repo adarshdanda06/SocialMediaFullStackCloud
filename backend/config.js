16c1a79d93d509b2ac898c9af0308bea14d18081
Adarsh Danda <adarsh.danda1@gmail.com>
2025-01-18 18:13:07 -0800

    added CD pipeline and reconfigured env variables

---
+++ backend/config.js
@@ -0,0 +1,22 @@
+const accessKey = process.env.ACCESS_KEY_ID
+const secretAccessKey = process.env.SECRET_ACCESS_KEY
+
+let awsConfig = {
+    'region': 'us-east-2',
+    'accessKeyId': accessKey,
+    'secretAccessKey': secretAccessKey,
+};
+
+let s3AwsConfig = {
+    'region': 'us-east-2',
+    'accessKeyId': accessKey,
+    'secretAccessKey': secretAccessKey,
+}
+
+let dynamoTableName = "dynamoSocialSchema";
+
+module.exports = {
+    awsConfig,
+    s3AwsConfig,
+    dynamoTableName
+}
